ef41ba15f7a193282a0823aab32d1f7ccce47b3a
ptpofficialxd <85019026+ptpofficialxd@users.noreply.github.com>
2022-08-18 00:39:45 +0700

    Update WordMapPageTask.java

--- src/main/java/se233/chapter3/controller/WordMapPageTask.java
+++ src/main/java/se233/chapter3/controller/WordMapPageTask.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.toMap;
 
 public class WordMapPageTask implements Callable<Map<String, FileFreq>> {
@@ -25,8 +26,8 @@ public class WordMapPageTask implements Callable<Map<String, FileFreq>> {
         Pattern pattern = Pattern.compile(" ");
         String s = reader.getText(doc.getDocument());
         wordCount = pattern.splitAsStream(s)
-                .map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim())
-                .filter(word -> word.length() > 3)
+                .map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim()) //ทั้งหมดตัวเล็ก
+                .filter(word -> word.length() > 3) //นับแค่คำที่ยาวกว่า 3 ตัวอักษร
                 .map(word -> new AbstractMap.SimpleEntry<>(word, 1))
                 .collect(toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue, Integer::sum))
                 .entrySet()
